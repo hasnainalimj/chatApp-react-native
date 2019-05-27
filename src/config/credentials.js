6492c8b36fe7977e730b09f0f6b3e3a911c334e5
hasnainalimj <40880679+hasnainalimj@users.noreply.github.com>
2019-05-27 00:52:48 -0700

    Update credentials.js

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,9 +1,3 @@
 export const firebaseCredentials = {
-  apiKey: "AIzaSyBVfrQJu6GOQRcc4djNZcgndsrXY5Q5UTM",
-  authDomain: "chat-app-561ae.firebaseapp.com",
-  databaseURL: "https://chat-app-561ae.firebaseio.com",
-  projectId: "chat-app-561ae",
-  storageBucket: "chat-app-561ae.appspot.com",
-  messagingSenderId: "353900786744",
-  appId: "1:353900786744:web:429f9c2415440a0a"
+ 
 };
